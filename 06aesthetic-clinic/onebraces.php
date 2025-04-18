9479cfdbf6d4cc5a1361fedb7822060defea7d6d
sim seonmi <93301892+s2meeeee@users.noreply.github.com>
2025-04-18 16:45:05 +0900

    250418

---
+++ 06aesthetic-clinic/onebraces.php
@@ -0,0 +1,132 @@
+<?php include $_SERVER['DOCUMENT_ROOT'].'/include/head.php'; ?>
+<div id="wrapper">
+  	<div class="inner2">
+		<div class="subTitle01">
+			ONE 치아 교정  “밝은 미소를 찾아드립니다.”
+		</div>
+	  	
+		<div class="subBox01 subBox0605_1 bgimg01">
+		  <dt><b style="color:#4c80cd;">치아 교정</b>이 필요한 이유는?</dt>
+			<dl>첫 번째, 삐뚤어진 치열은 칫솔질이 잘 되지 않으며 충치와 잇몸 질환에 <br class="web">
+                노출될 가능성이 높습니다. 두 번째, 고르지 못한 치열은 안모에 영향을<br class="web">
+                끼칠 수 있습니다. 세 번째, 부정교합으로 인한 정확한 발음 및 저작에 <br class="web">
+                어려움이 있을 수 있습니다. <b style="color:#0158f3;">올바른 위치의 치아는 구강 위생을 <br class="web">증진시키고 충치와 치주염 등 치아와 관련된 질환의 발생 가능성을<br class="web">
+                줄여줍니다.</b>
+            </dl>
+		</div>	<!--치아교정이 필요한 이유-->
+		
+		<div class="subTitle01">
+			<b style="color:#4c80cd;">치아 교정 </b>종류 및 장점
+		</div>
+		
+        <div class="subBox01 subBox0605_2 bgimg02">
+		  <dt><b style="color:#4c80cd;">메탈 교정</b>이란?</dt>
+			<dl>메탈교정은 가장 오랫동안 사용되어온 교정 장치로 가격 대비 효율성이<br class="web">
+                좋아 꾸준히 사용되고 있습니다. 심하게 벌어진 이나 개방교합, 반대교합<br class="web">
+                등에 사용되며 마찰력이 우수한 편에 속해 확실한 교정을 원할시 진행하는 <br class="web">
+                교정입니다. 치아에 부착되는 브라켓이 금속재질로 이루어져 있어<br class="web">
+                심미성이 떨어진다는 점이 있습니다.</dl>
+		</div> <!--메탈 교정-->
+        
+        <div class="subBox01 subBox0605_3 bgimg03">
+		  <dt><b style="color:#4c80cd;">클리피씨 교정</b>이란?</dt>
+			<dl>클리피씨교정은 자가결찰방식을 이용한 방식으로 마찰력이 줄어<br class="web">
+                치아의 이동속도가 빠른편이며 통증이 적은편에 속합니다.<br class="web">
+                브라켓은 세라믹교정과 유사하나 장치 자체 뚜껑이 여닫이 방식으로 <br class="web">
+                되어 와이어를 쉽게 연결할 수 있으며, 교정치료로 잦은 내원이<br class="web">
+                어렵거나 덧니가 심하신 분들에게 용이한 교정입니다.</dl>
+		</div> <!--클리피씨 교정-->
+        
+        <div class="subBox01 subBox0605_4 bgimg04">
+		  <dt><b style="color:#4c80cd;">콤비 교정</b>이란?</dt>
+			<dl>콤비교정은 상악, 하악 치아상태에 따라 다른 장치를 사용해 심미성을<br class="web">
+                높이고 비용은 절감할 수 있는 교정방법입니다. 웃거나 말할 때 <br class="web">
+                상대적으로 잘 보이는 윗니에 설측 부착, 노출이 적은 아랫니에 순측으로<br class="web">
+                경제성을 동시에 갖고 있어 눈에 보이는 교정 장치가 부담되는 분들에게<br class="web">
+                용이한 교정방법입니다.</dl>
+		</div> <!--콤비 교정-->        
+        
+        
+        <div class="subBox01 subBox0605_5 bgimg05">
+		  <dt><b style="color:#4c80cd;">설측 교정</b>이란?</dt>
+			<dl>설측교정은 치아에 고정하는 브라켓과 연결하는 와이어가 안으로 <br class="web">
+                부착되는 교정방식으로 심미적인 효과를 높인 교정으로 기존의 <br class="web">
+                교정방식과는 차이가 있으며, 별도 기공 과정으로 환자에게 맞는 <br class="web">
+                부담스러우신 분들에게 용이한 교정방법입니다.
+            </dl>
+		</div> <!--설측 교정-->
+        
+        
+        <div class="subBox01 subBox0605_6 bgimg06">
+		  <dt><b style="color:#4c80cd;">투명 교정</b>이란?</dt>
+			<dl>투명 교정이란 특수 재질의 투명한 강화 플라스틱으로 제작된 장치를<br class="web">
+                치아에 착용하여 치아를 이동시키는 교정 치료 방법입니다.<br class="web">
+                이 장치는 플라스틱 재질로 되어 있어 환자 스스로 탈부착이 가능하여<br class="web">
+                편리합니다. 전문의가 환자의 입에 맞게 교정용 트레이를 설계 및 제작하며<br class="web">
+                투명한 소재로 제작되어 심미성이 뛰어납니다.
+            </dl>
+		</div> <!--투명 교정-->
+        
+		<!--<div class="navBox02">
+			<ul>
+				<li><img src="https://onedentalad.co.kr/img/04navigation-implant/nav_img12.png"  alt=""/>
+					<dt>빠른 회복</dt>
+					<dl>최소절개 시술로 출혈없이 <br class="web"> 빠른 회복이 가능합니다</dl>
+				</li>
+				<li><img src="https://onedentalad.co.kr/img/04navigation-implant/nav_img13.png"  alt=""/>
+					<dt>감염 위험 감소</dt>
+					<dl class="t1">염증 및 감염의 위험이 적습니다</dl>
+				</li>
+				<li><img src="https://onedentalad.co.kr/img/04navigation-implant/nav_img14.png"  alt=""/>
+					<dt>정확한 치료계획</dt>
+					<dl>골 조직과 신경위치를 파악하여  <br class="web">정확한 치료 계획을 수립합니다</dl>
+				</li>
+				<li><img src="https://onedentalad.co.kr/img/04navigation-implant/nav_img15.png"  alt=""/>
+					<dt>1:1 맞춤</dt>
+					<dl class="t1">1:1 맞춤형 유도장치로 진행합니다</dl>
+				</li>
+				<li><img src="https://onedentalad.co.kr/img/04navigation-implant/nav_img16.png"  alt=""/>
+					<dt>전신질환 환자 가능</dt>
+					<dl>수술시간 단축으로 전신질환  <br class="web">환자분에게 권해드립니다</dl>
+				</li>
+				<li><img src="https://onedentalad.co.kr/img/04navigation-implant/nav_img17.png"  alt=""/>
+					<dt>내원 횟수 단축</dt>
+					<dl>내원횟수 및 임플란트  <br class="web">완성 기간이 단축됩니다</dl>
+				</li>
+			</ul>		
+		</div>-->
+	</div>	<!--inner1-->
+	
+    
+    
+	<div class="navBox01">
+		<div class="inner2">
+			<div class="subTitle01">이런 분들께 추천드려요!</div>
+			
+			<ul>
+				<li><img src="https://onedentalad.co.kr/img/04navigation-implant/bra_img08.png"  alt=""/>
+					<dl>POINT 01</dl>
+					<dt>빠른 교정을 원하는 분</dt>
+					<dd>결혼이나 취업 등 중요한 일정을 앞두고 빠른 인상 개선과 자신감을 찾길 원하는 분</dd>
+				</li>
+				<li><img src="https://onedentalad.co.kr/img/04navigation-implant/bra_img09.png"  alt=""/>
+					<dl>POINT 02</dl>
+					<dt>구강 건강 개선이 필요한 분</dt>
+					<dd>부정교합으로 인해 치아에 음식물이 잘 끼거나 충치나 치주염 발생 가능성이 높은 분</dd>
+				</li>
+				<li><img src="https://onedentalad.co.kr/img/04navigation-implant/bra_img10.png"  alt=""/>
+					<dl>POINT 03</dl>
+					<dt>구강 기능 개선이 필요한 분</dt>
+					<dd>부정교합으로 인해 식사나 발음에 불편함을 겪고 있는 분</dd>
+				</li>
+				<li><img src="https://onedentalad.co.kr/img/04navigation-implant/bra_img11.png"  alt=""/>
+					<dl>POINT 04</dl>
+					<dt>치아교정을 고민하는 누구나</dt>
+					<dd>성장기 어린이부터 성인까지 치아교정을 통해 밝은 미소와 구강건강 개선을 원하는 분</dd>
+				</li>
+			</ul>
+		</div>
+	</div>
+	
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/include/bottom.php'; ?>
---
+++ 06aesthetic-clinic/oneqickbraces.php
@@ -0,0 +1,159 @@
+<?php include $_SERVER['DOCUMENT_ROOT'].'/include/head.php'; ?>
+<div id="wrapper">
+  <div class="inner2">
+		<div class="tooBox01">
+			평균 2~3년 소요되는 교정 기간을<br class="web">
+            1년으로 단축시켜 심미성과 효율성을 한 번에!<br class="web">
+            <b style="color:#0158f3">“원치과 ONE 교정”</b>
+		</div> <!--상단 설명-->
+		
+		<div class="subTitle01 tooBox02">
+			ONE 교정<b style="color:#1a2a71">이란?</b>
+		</div>
+		
+        <div class="tooBox03">
+            <ul>
+                <li>
+                    <img src="https://onedentalad.co.kr/img/06aesthetic-clinic/onebr01.jpg">
+                    <dt>1</dt>
+                    <dl>장점만 모아 창안된 교정법</dl>
+                    <dd>ONE 치아교정은 기존 교정법들의<br class="web">
+                        장점을 모아 창안된 교정법입니다.
+                    </dd>
+                </li>
+                
+                <li>
+                    <img src="https://onedentalad.co.kr/img/06aesthetic-clinic/onebr02.jpg">
+                    <dt>2</dt>
+                    <dl>단기간 집중 케어</dl>
+                    <dd>월 3회 가량의 검진과 치료를 통해<br class="web">
+                        치아를 둘러싸고 있는 잇몸뼈(치조골)를<br class="web">
+                        보존한 상태로 치아 이동을 가속화하는 방식
+                    </dd>
+                </li>
+                
+                <li>
+                    <img src="https://onedentalad.co.kr/img/06aesthetic-clinic/onebr03.jpg">
+                    <dt>3</dt>
+                    <dl>보다 빨라진 교정기간</dl>
+                    <dd>평균 2-3년의 소요 기간이<br class="web">
+                        발생하는 교정을 1년으로 단축시켰습니다.
+                    </dd>
+                </li>
+            </ul>
+        </div>    
+   </div>
+
+	
+    <div class="tooBox05">
+        <div class="inner">
+            <div class="subTitle01 tooBox02">
+			ONE 교정 <b style="color:#1a2a71">장점</b>
+		    </div>
+            
+            <ul>
+                <li>
+                    <dl>01</dl>
+                    <img src="https://onedentalad.co.kr/img/06aesthetic-clinic/onebr04.jpg">
+                    <dt>저작 기능 개선</dt>
+                    <dd>부정 교합으로 불편했던 저작 기능을</dd>
+                    <dd>개선하여 교정 전보다 편하게 식사를</dd>
+                    <dd>하실수 있습니다.</dd>
+                </li>
+                <li>
+                    <dl>02</dl>
+                    <img src="https://onedentalad.co.kr/img/06aesthetic-clinic/onebr05.jpg">
+                    <dt>편리한 구강관리 </dt>
+                    <dd>덧니를 없앤 가지런한 치아 배열은</dd>
+                    <dd>음식물이 덜 껴 충치 예방과 구강 관리가</dd>
+                    <dd>상대적으로 수월합니다.</dd>
+                </li>
+                <li>
+                    <dl>03</dl>
+                    <img src="https://onedentalad.co.kr/img/06aesthetic-clinic/onebr06.jpg">
+                    <dt>발음 문제 개선</dt>
+                    <dd>가지런한 치아 배열은 교정 전 덧니</dd>
+                    <dd>사이로 발음이 세는 현상을 개선하여</dd>
+                    <dd>원활한 의사소통을 돕습니다.</dd>
+                </li>
+                <li>
+                    <dl>04</dl>
+                    <img src="https://onedentalad.co.kr/img/06aesthetic-clinic/onebr07.jpg">
+                    <dt>인상 개선</dt>
+                    <dd>고른 치아는 밝고 깔끔한 인상을 주어</dd>
+                    <dd>취업, 결혼 준비 등 중요한 행사 시</dd>
+                    <dd>긍정적인 효과를 줍니다.</dd>
+                </li>
+            </ul>
+        </div>
+    </div> <!--두 번째 단락-->
+    
+    <div class="inner2">
+        <div class="tooBox06">
+             <div class="subTitle01 tooBox02">
+			    ONE 교정 <b style="color:#1a2a71">Before&amp;After</b>
+                 <dl>원치과의원 교정과 전문의의 노하우로 완성하는 맞춤 ONE 교정</dl>
+		    </div>
+            
+            <ul>
+                <li>
+                    <img src="https://onedentalad.co.kr/img/06aesthetic-clinic/onebr08.jpg">
+                    <dt>Before</dt>
+                </li>
+                <li>
+                    <img src="https://onedentalad.co.kr/img/06aesthetic-clinic/onebr09.jpg">
+                    <dt>After</dt>
+                </li>
+                <li>
+                    <img src="https://onedentalad.co.kr/img/06aesthetic-clinic/onebr10.jpg">
+                    <dt>Before</dt>
+                </li>
+                <li>
+                    <img src="https://onedentalad.co.kr/img/06aesthetic-clinic/onebr11.jpg">
+                    <dt>After</dt>
+                </li>
+            </ul>
+
+        </div>
+    </div> <!--Before After-->
+    
+    <div class="tooBox07">
+        <div class="inner2">
+            <div class="subTitle01 tooBox02 tooBox02b">
+			    ONE 교정 중 <b>주의사항</b>
+                <dl>친절한 상담과 진단부터 치료까지, ONE 교정</dl>
+		    </div>
+            
+            <dd><span>1</span>
+                교정 장치가 탈락 할 수 있으므로 마른 오징어, 엿 등 딱딱한 음식은 피하세요. 
+            </dd>
+            <dd><span>2</span>
+                교정용 칫솔과 치간 칫솔을 사용하여 교정 장치 사이를 청결하게 관리하세요.
+            </dd>
+            <dd><span>3</span>
+                앞니의 뿌리는 어금니에 비해 약하므로 교정 중에는 앞니에 힘 주는걸 삼가하세요.
+            </dd>
+            <dd><span>4</span>
+                교합을 맞추기 위해 사용하는 고무줄을 소홀히 할 경우 치료가 지연될 수 있습니다.
+            </dd>
+            <dd><span>5</span>
+                정기적으로 내원하셔서 스케일링과 충치검진을 받으시길 권장합니다.
+            </dd>
+        </div>
+    </div>    <!--주의사항-->
+	
+    <div class="tooBox08 labbgm08 oneqickbraces-bg__img">
+        <div class="inner2">
+            <div class="subTitle01 tooBox02">
+			    <b style="color:#1c3271">ONE 교정 </b>이런 분들에게 추천드려요!
+                <dl>환자분들의 만족스러운 결과를 위해 최선을 다합니다.</dl>
+		    </div>
+            <dd><span>Case 1.</span>돌출된 덧니로 입술이 부각되어 보이는 분</dd>
+            <dd><span>Case 2.</span>부정 교합으로 일상생활이 불편한 분</dd>
+            <dd><span>Case 3.</span>단 기간 만족스러운 치아교정을 원하는 분</dd>
+            <dd><span>Case 4.</span>얼굴 인상을 개선하고 싶으신 분</dd>
+         </div>
+    </div> <!--추천 대상--> 
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/include/bottom.php'; ?>
